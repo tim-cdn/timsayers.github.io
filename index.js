7d534a0861a2b461241dbdf3e7e7e2c42bec913e
TimSayers <timasayers@gmail.com>
2016-03-02 13:53:01 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+var Div = document.getElementById('gp');
 function hideGP(){
+    Div.setAttribute('class', 'hidden');
     $(".gp").hide();
     document.getElementById("gp").style.visibility = "hidden";
 }
 function showGP(){
+    Div.setAttribute('class', 'show');
     $(".gp").show();
     document.getElementById("gp").style.visibility = "visible";
 }
