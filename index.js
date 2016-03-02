b5ea34a5192b6de1d7fa21fac9c9568ba5bb521e
TimSayers <timasayers@gmail.com>
2016-03-02 14:54:55 +1100

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,4 @@
     function show(){
         document.getElementById('gp').hidden = false;
     }
- function toggle_visibility(id) 
-    {
-        var e = document.getElementById(id);
-        if ( e.style.display == 'block' )
-            e.style.display = 'none';
-        else
-            e.style.display = 'block';
-    }
+
